d00d7978809234c5660929c1edd342a32b95134a
Seew0 <96205648+Seew0@users.noreply.github.com>
2022-04-09 20:24:03 +0000

    first commit

---
+++ encryption.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+
+import argparse
+import bcrypt
+
+
+
+parser = argparse.ArgumentParser(description='Password Encryption Tool v1.0')
+
+parser.add_argument('-v','--version',action='version',version='Password Encryption Tool v1.0')
+
+parser.add_argument('passwd',type=str,help="The password to analyze")
+
+args= parser.parse_args()
+
+passwd= args.passwd
+
+encode_passwd=passwd.encode('utf-8')
+
+encrypted_passwd=bcrypt.hashpw(encode_passwd,bcrypt.gensalt())
+print(encrypted_passwd)
+
+
---
+++ vulnerablecheck.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import argparse
+import requests
+
+
+p=argparse.ArgumentParser(description='vulnerability check')
+p.add_argument('-v','--version',action='version',version='v1.0',help='version of tool')
+p.add_argument('url',type=str,help='enter your url with scheme')
+args=p.parse_args()
+
+url=args.url
+
+
+response = requests.get(url)
+
+if(response.status_code==200):
+    response= requests.get(url+ '/wp-admin')
+    
+    if(response.status_code==404):
+        print('Admin page do not exist or hidden for this site(not a vulnerable site)')
+    elif(response.status_code==200):
+        print('Vulnerable site')
+    else:
+
+        print('not a vulnerable site')
+else:
+    print('request failure')
